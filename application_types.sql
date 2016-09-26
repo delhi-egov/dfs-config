28b1866f5ace01ec513c82804c72a9aa15554403
Vaibhav Sinha <vaibhavsinh@gmail.com>
2016-09-27 01:23:39 +0530

    Configs for DFS setup

---
+++ application_types.sql
@@ -0,0 +1,1 @@
+INSERT INTO `application_types` VALUES (1,'2016-09-21 10:06:54','2016-09-21 10:06:59','Granting-of-Fire-Safety-Certificate','Granting-of-Fire-Safety-Certificate',0.00,'\0'),(2,'2016-09-21 10:06:54','2016-09-21 10:06:59','process','Renewal-of-Fire-Safety-Certificate',0.00,'\0'),(3,'2016-09-21 10:06:54','2016-09-21 10:06:59','process','Declaration-by-Owner/Occupier',0.00,'\0');
